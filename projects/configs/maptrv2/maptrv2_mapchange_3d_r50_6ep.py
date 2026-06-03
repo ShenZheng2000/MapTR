324bfb73baf6af71e94ccd336842e380208efc7e
Shen Zheng <shenzhen@trinity-0-18.eth>
2026-06-03 19:51:04 -0400

    mapchange config & clear test/train.sh for nuscenes runs

---
+++ projects/configs/maptrv2/maptrv2_mapchange_3d_r50_6ep.py
@@ -0,0 +1,25 @@
+_base_ = [
+    'maptrv2_av2_3d_r50_6ep.py',
+]
+
+
+# NOTE: add data_root here and use it as pose_root
+data_root = '/scratch/shenzhen/datasets/mapchange/' # NOTE: change to geosplit data
+
+data = dict(
+    samples_per_gpu=3, # reduce to 3 to avoid oom
+    train=dict(
+        data_root=data_root,
+        ann_file=data_root + 'av2_map_infos_train.pkl',
+        ),
+    val=dict(
+        data_root=data_root,
+        ann_file=data_root + 'av2_map_infos_val.pkl',
+        map_ann_file=data_root + 'av2_gt_map_anns_val.json',
+    ),
+    test=dict(
+        data_root=data_root,
+        ann_file=data_root + 'av2_map_infos_val.pkl',
+        map_ann_file=data_root + 'av2_gt_map_anns_val.json',
+    )
+)
--- train.sh
+++ train.sh
@@ -11,3 +11,15 @@
 #     ./projects/configs/maptrv2/maptrv2_av2_3d_r50_6ep_geosplit.py \
 #     8 \
 #     --work-dir work_dirs/maptrv2_av2_3d_r50_6ep_geosplit
+
+# # # # => train maptrv2 on map change
+# bash ./tools/dist_train.sh \
+#     ./projects/configs/maptrv2/maptrv2_mapchange_3d_r50_6ep.py \
+#     8 \
+#     --work-dir work_dirs/maptrv2_mapchange_3d_r50_6ep
+
+# # # => train maptrv2 on geosplit (nuscenes)
+bash ./tools/dist_train.sh \
+    ./projects/configs/maptrv2/maptrv2_nusc_r50_24ep.py \
+    8 \
+    --work-dir work_dirs/maptrv2_nusc_r50_24ep
