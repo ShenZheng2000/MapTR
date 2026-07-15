c91c26fd596d5706386f466107cf88bd0b331b85
Shen Zheng <shenzhen@trinity-3-8.eth>
2026-07-15 19:19:39 -0400

    add centerline; TODO eval gt zero

--- train.sh
+++ train.sh
@@ -19,6 +19,10 @@
 # train (mobilenet backbone; 100m x 50m)
 # bash tools/dist_train.sh projects/configs/maptrv2/maptrv2_av2_3d_mobilenetv3_6ep_geosplit_100x50.py 8
 
+# train (w/ centerline)
+# bash tools/dist_train.sh projects/configs/maptrv2/maptrv2_av2_3d_r50_6ep_geosplit_w_centerline.py 8
+
+
 
 # test
 # bash tools/dist_test_map.sh \
@@ -58,7 +62,7 @@
 # bash tools/dist_train.sh projects/configs/maptrv2/maptrv2_nusc_r50_24ep_w_centerline.py 8
 
 # train (mobilenet backbone w/ centerline)
-bash tools/dist_train.sh projects/configs/maptrv2/maptrv2_nusc_mobilenetv3_24ep_w_centerline.py 8
+# bash tools/dist_train.sh projects/configs/maptrv2/maptrv2_nusc_mobilenetv3_24ep_w_centerline.py 8
 
 
 # test 
